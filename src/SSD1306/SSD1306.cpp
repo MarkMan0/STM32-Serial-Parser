6bd718fd3645757b3f6e16e9062d47bcfe67320e
MarkMan0 <pal.mark28@gmail.com>
2021-09-11 16:11:55 +0200

    Rework SSD1306::begin

--- src/SSD1306/SSD1306.cpp
+++ src/SSD1306/SSD1306.cpp
@@ -14,81 +14,38 @@
 bool SSD1306::begin() {
   // set mux
   namespace reg = SSD_1306_reg;
-  constexpr uint8_t control_byte{ 0 };
-  uint8_t buff[3];
-  bool res{ true };
 
-  auto write = [&res, &buff](size_t len) { res = res && i2c.write(addr_, buff, len); };
+  static constexpr uint8_t config[] = { // turn off display
+                                        reg::SET_DISPLAY_OFF,
+                                        // Display height - 1
+                                        reg::SET_MUX_RATIO, 63,
+                                        // display vertical shift
+                                        reg::SET_DISPLAY_OFFSET, 0,
+                                        // display start line is 0
+                                        reg::SET_DISPLAY_START_LINE,
+                                        // segment remap 1 - flip in X
+                                        reg::SET_SEGMENT_REMAP | 0x1,
+                                        // normal COM scan
+                                        reg::SET_COM_OUTPUT_SCAN_DIR,
+                                        // COM pins hardware layout
+                                        reg::SET_COM_HW_CONFIG, 0x2 | (0x1 << 4) | (0x0 << 5),
+                                        // set display contrast/brightness
+                                        reg::SET_CONTRAST_CONTROL, 100,
+                                        // use ram to display
+                                        reg::ENTIRE_DISPLAY_FROM_RAM,
+                                        // 1 in RAM means OLED on
+                                        reg::SET_NORMAL_DISPLAY,
+                                        // set oscillator from datasheet
+                                        reg::SET_CLOCK_DIVIDE_RATIO, (0b1000 << 4),
+                                        // enable charge pump
+                                        reg::CHARGE_PUMP_SETTINGS, 0x10 | (0x1 << 2),
+                                        // set addressing mode
+                                        SSD_1306_reg::SET_MEMORY_ADDRESSING_MODE, 0x01,
+                                        // turn on display
+                                        reg::SET_DISPLAY_ON
+  };
 
-  buff[0] = control_byte;
-
-  // Turn the display off for config
-  buff[1] = reg::SET_DISPLAY_OFF;
-  write(2);
-
-  // Display height - 1
-  buff[1] = reg::SET_MUX_RATIO;
-  buff[2] = 63;
-  write(3);
-
-  // display vertical shift
-  buff[1] = reg::SET_DISPLAY_OFFSET;
-  buff[2] = 0;
-  write(3);
-
-  // display start line is 0
-  buff[1] = reg::SET_DISPLAY_START_LINE;
-  write(2);
-
-  // segment remap 1 - flip in X
-  buff[1] = reg::SET_SEGMENT_REMAP | 0x1;
-  write(2);
-
-  // normal COM scan
-  buff[1] = reg::SET_COM_OUTPUT_SCAN_DIR;
-  write(2);
-
-  // COM pins hardware layout, change bits
-  // bit 4: com pin config
-  // bit 5: left/right remap
-  buff[1] = reg::SET_COM_HW_CONFIG;
-  buff[2] = 0x2 | (0x1 << 4) | (0x0 << 5);
-  write(3);
-
-  // set display contrast/brightness
-  buff[1] = reg::SET_CONTRAST_CONTROL;
-  buff[2] = 100;
-  write(3);
-
-  // use ram to display
-  buff[1] = reg::ENTIRE_DISPLAY_FROM_RAM;
-  write(2);
-
-  // 1 in RAM means OLED on
-  buff[1] = reg::SET_NORMAL_DISPLAY;
-  write(2);
-
-  // set oscillator from datasheet
-  buff[1] = reg::SET_CLOCK_DIVIDE_RATIO;
-  buff[2] = (0b1000 << 4);
-  write(3);
-
-  // enable charge pump
-  buff[1] = reg::CHARGE_PUMP_SETTINGS;
-  buff[2] = 0x10 | (0x1 << 2);
-  write(3);
-
-  // set addressing mode
-  buff[1] = SSD_1306_reg::SET_MEMORY_ADDRESSING_MODE;
-  buff[2] = 0x01;
-  write(3);
-
-  // turn on display only if everything went OK
-  if (res) {
-    buff[1] = reg::SET_DISPLAY_ON;
-    write(2);
-  }
-  return res;
+  return i2c.write_register(addr_, 0, const_cast<uint8_t*>(config), sizeof(config));
 }
 
 bool SSD1306::draw_canvas(GFX::canvas_t& canvas) {
